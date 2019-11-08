7ad763037db49008f55c2a65b94e0938daaae61f
Mellor <s_mellor@ats-hr420-dm11.fc.ca>
2019-11-08 13:27:57 -0500

    added it

---
+++ gameFunctions/gameVars.py
@@ -0,0 +1,16 @@
+from random import randint
+
+# 'basket' of choices
+choices=["rock","paper","scissors"]
+
+# adding lives --> when one of the other gets to 0, win / lose
+player_lives = 5
+computer_lives = 5
+
+total_lives= 5
+
+# let the ai make a choice
+computer=choices[randint(0,2)]
+
+# setup a game loop here so we dont have to keep restarting
+player = False
