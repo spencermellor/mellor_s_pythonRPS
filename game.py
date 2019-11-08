f176db9bda19aa708522889ce531c457d3c59863
Mellor <s_mellor@ats-hr420-dm11.fc.ca>
2019-11-08 14:00:38 -0500

    fixed it up again HW2

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from random import randint
-from gameFunctions import winlose, gameVars
+from gameFunctions import winlose, gameVars, compare
 
 while gameVars.player is False:
 	print("====================================\n")
@@ -11,41 +11,11 @@ while gameVars.player is False:
 
 	#start doing some logic and condition checking
 	print("computer: ", gameVars.computer, "player: ", gameVars.player)
-
-	#always check a breaking condition first
-	if player == gameVars.computer:
-		# we have a tie, no point in going any further
-		print("tie, no one wins! try again")
-
-	elif player == "quit":
-		print ("you chose to quit, quitter.")
-		exit()
-
-	elif player == "rock":
-		if gameVars.computer == "paper":
-			print("You lose!", gameVars.computer, "covers", player, "\n")
-			gameVars.player_lives = gameVars.player_lives -1
-		else:
-			print("You won!", player, "smashes", gameVars.computer, "\n")
-			gameVars.computer_lives = gameVars.computer_lives -1
-
-	elif player == "paper":
-		if gameVars.computer == "scissors":
-			print("You lose!", gameVars.computer, "slices", player, "\n")
-			gameVars.player_lives = gameVars.player_lives -1
-		else:
-			print("You won!", player, "covers", gameVars.computer, "\n")
-			gameVars.computer_lives = gameVars.computer_lives -1
-
-
-	elif player == "scissors":
-		if gameVars.computer == "rock":
-			print("You lose!", gameVars.computer, "smashes", player, "\n")
-			gameVars.player_lives = gameVars.player_lives -1
-		else:
-			print("You won!", player, "slices", gameVars.computer, "\n")
-			gameVars.computer_lives = gameVars.computer_lives -1
 	
+	# check the function
+
+	compare.compare (player)
+
 	if gameVars.player_lives is 0:
 		winlose.winorlose("lost")
 
